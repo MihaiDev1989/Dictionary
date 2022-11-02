6934ff1f3f6b6f4c23b4d8190fe6209524de34b5
MihaiDev1989 <mihai.vasile19893010@gmail.com>
2022-11-02 22:09:47 +0200

    Dictionary files added

---
+++ script.js
@@ -0,0 +1,47 @@
+let addWordBtn = document.getElementById("add-word");
+let searchWordBtn = document.getElementById("search-word");
+let listOfWords = [];
+
+addWordBtn.addEventListener('click', function () {
+    let inputElement = document.getElementById("word");
+    let word = inputElement.value;
+    if (inputElement.value !== "") {
+        let messageElement = document.getElementsByClassName("succes-submited-message");
+        if (!messageElement[0].classList.contains("succes-alert-message")) {
+            messageElement[0].style.display = "block";
+            setTimeout(function () {
+                messageElement[0].style.display = "none";
+            }, 1000);
+        } else {
+            messageElement[0].style.display = "none";
+        }
+        listOfWords.push(word);
+    } else {
+        let messageElement = document.getElementsByClassName("danger-submited-message");
+        if (!messageElement[0].classList.contains("succes-alert-message")) {
+            messageElement[0].style.display = "block";
+            setTimeout(function () {
+                messageElement[0].style.display = "none";
+            }, 1000);
+        } else {
+            messageElement[0].style.display = "none";
+        }
+    }
+});
+
+searchWordBtn.addEventListener('click', function () {
+    let inputElement = document.getElementById("word");
+    let word = inputElement.value;
+    word.toLowerCase();
+    let exists = false;
+    listOfWords.forEach(function (item) {
+        if (item === word) {
+            exists = true;
+        }
+    });
+    if (exists) {
+        document.getElementById("existing-text-message-content").innerHTML = '<p class="alert alert-success">Your word is in the list!</p>';
+    } else {
+        document.getElementById("existing-text-message-content").innerHTML = '<p class="alert alert-danger">Your word is not in the list!</p>';
+    }
+});
